a69969b2b33415517e0744d67713ad32066c0cab
jordan-laird <jordanmack.laird@gmail.com>
2018-12-17 13:39:31 -0600

    Done

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const BotCard = props => {
+  
   const { bot } = props;
-
   let botType;
 
   switch (bot.bot_class) {
@@ -19,12 +19,13 @@ const BotCard = props => {
       botType = <div />;
   }
 
+
   return (
     <div className="ui column">
       <div
         className="ui card"
         key={bot.id}
-        onClick={() => console.log("add code to connect event listener")}
+        onClick={(e) => props.handleClick(bot)}
       >
         <div className="image">
           <img alt="oh no!" src={bot.avatar_url} />
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,53 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
+import BotSpecs from '../components/BotSpecs'
+let toggleShow = false;
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  constructor() {
+    super()
+    this.state = {
+      bots: [],
+    }
+  }
+
+  fetchBots = () => {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+      .then(resp => resp.json())
+      .then((bots) => {
+        this.setState({ bots: bots })
+
+      })
+  }
+
+  addBotToArmy = (botId) => {
+    this.setState((state) => {
+      let bot = state.bots.find(bot => bot.id === botId)
+      bot.inArmy = !bot.inArmy
+      return state;
+    })
+
+  }
+
+  handleClick = (bot) => {
+    let botId = bot.id
+    this.addBotToArmy(botId)
+    console.log(botId)
+
+  }
+
+  componentWillMount() {
+    this.fetchBots()
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        {<YourBotArmy bots={this.state.bots} addBotToArmy={this.addBotToArmy} handleClick={this.handleClick} />}
+        {toggleShow ? <BotSpecs bots={this.state.bots} /> :
+          <BotCollection bots={this.state.bots} addBotToArmy={this.addBotToArmy} handleClick={this.handleClick} />}
       </div>
     );
   }
