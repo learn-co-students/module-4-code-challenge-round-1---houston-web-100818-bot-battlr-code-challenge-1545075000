5807592d29ff0698165bc9a6b4ba65368f4429a1
ch0mper <hannahchang17@gmail.com>
2018-12-17 13:39:32 -0600

    checkpoint

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,62 +1,51 @@
 import React from "react";
 
-const BotCard = props => {
-  const { bot } = props;
+class BotCard extends React.Component {
 
-  let botType;
-
-  switch (bot.bot_class) {
-    case "Assault":
-      botType = <i className="icon military" />;
-      break;
-    case "Defender":
-      botType = <i className="icon shield" />;
-      break;
-    case "Support":
-      botType = <i className="icon ambulance" />;
-      break;
-    default:
-      botType = <div />;
+  handleClick = () => {
+    let botID = this.props.bot.id
+    this.props.addToArmy(botID)
   }
 
-  return (
-    <div className="ui column">
-      <div
-        className="ui card"
-        key={bot.id}
-        onClick={() => console.log("add code to connect event listener")}
-      >
-        <div className="image">
-          <img alt="oh no!" src={bot.avatar_url} />
-        </div>
-        <div className="content">
-          <div className="header">
-            {bot.name} {botType}
+  render(){
+    return (
+      <div className="ui column">
+        <div
+          className="ui card"
+          key={this.props.bot.id}
+          onClick={() => this.handleClick()}
+        >
+          <div className="image">
+            <img alt="oh no!" src={this.props.bot.avatar_url} />
           </div>
+          <div className="content">
+            <div className="header">
+              {this.props.bot.name}
+            </div>
 
-          <div className="meta text-wrap">
-            <small>{bot.catchphrase}</small>
+            <div className="meta text-wrap">
+              <small>{this.props.bot.catchphrase}</small>
+            </div>
           </div>
-        </div>
-        <div className="extra content">
-          <span>
-            <i className="icon heartbeat" />
-            {bot.health}
-          </span>
+          <div className="extra content">
+            <span>
+              <i className="icon heartbeat" />
+              {this.props.bot.health}
+            </span>
 
-          <span>
-            <i className="icon lightning" />
-            {bot.damage}
-          </span>
-          <span>
-            <i className="icon shield" />
-            {bot.armor}
-          </span>
+            <span>
+              <i className="icon lightning" />
+              {this.props.bot.damage}
+            </span>
+            <span>
+              <i className="icon shield" />
+              {this.props.bot.armor}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-  );
-
-};
+    );
+  }
+}
 
 export default BotCard;
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,41 @@
 import React from "react";
+import BotCollection from "../containers/BotCollection";
+import YourBotArmy from "../containers/YourBotArmy";
+
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+
+  state = {
+    bots: [],
+    armyBots: []
+  }
+
+  fetchBots = () => {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(res => res.json())
+    .then(bots => this.setState({bots}))
+  }
+
+  componentDidMount() {
+    this.fetchBots()
+  }
+
+  toggleBot = (botID) => {
+    if (!this.state.armyBots.find(bot => bot.id === botID)) {
+      let newArmyBot = this.state.bots.find(bot => bot.id === botID)
+      this.setState({ armyBots: [...this.state.armyBots, newArmyBot] })
+    }
+    else {
+      let newArmyBots = this.state.armyBots.filter(bot => bot.id !== botID)
+      this.setState({ armyBots: newArmyBots})
+    }
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        < YourBotArmy bots={this.state.armyBots} removeFromArmy={this.toggleBot}/>
+        < BotCollection bots={this.state.bots} addToArmy={this.toggleBot} />
       </div>
     );
   }
